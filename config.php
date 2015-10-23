e22c37273bcae09142f7429661e46bb02f9d301c
Andrey Romanov <andrey.a.romanov@gmail.com>
2015-10-23 13:26:00 +0300

    Reorder stations

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 $list=array("rock","nashe","best","nashe20","jazz","ultra");
-$names=array("best" => "Best FM","nashe" => "Наше радио","nashe20" => "Наше 2.0","rock" => "Rock FM","ultra" => "Ultra","jazz" => "Jazz");
+$names=array("rock" => "Rock FM","nashe" => "Наше радио","best" => "Best FM","nashe20" => "Наше 2.0","jazz" => "Jazz","ultra" => "Ultra");
 ?>
