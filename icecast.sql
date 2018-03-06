e57c0b2d89a72f3bb22fb96fbb9ed938077657d7
Andrey Romanov <andrey.a.romanov@gmail.com>
2018-03-06 19:41:09 +0300

    Fix encoding

--- icecast.sql
+++ icecast.sql
@@ -16,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- База данных: `icecast`
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `songs` (
   `station` varchar(100) NOT NULL,
   `song` varchar(100) NOT NULL,
   `user` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
